7602b243838ea30ff9d9e3cdfff2c4ae0101c4a1
Dushane <dushanesmith@gmail.com>
2023-04-02 12:58:20 -0400

    remove unnecessary live data function in articleListRepository and update paging gradle version

--- app/src/main/java/com/newscycle/data/ArticleListRepository.kt
+++ app/src/main/java/com/newscycle/data/ArticleListRepository.kt
@@ -1,20 +1,15 @@
 package com.newscycle.data
 
-import androidx.lifecycle.MutableLiveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.rxjava3.flowable
 import com.newscycle.Feed
 import com.newscycle.data.models.ArticleModel
-import com.newscycle.data.models.Results
 import io.reactivex.rxjava3.core.Flowable
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class ArticleListRepository(private val FEED_TAG: Feed) {
-    private val articleListLiveData: MutableLiveData<Results> = MutableLiveData()
 
-    @ExperimentalCoroutinesApi
     fun getArticles(query: String, fromDate: String, sortBy: String): Flowable<PagingData<ArticleModel>> {
         return Pager(
             config = PagingConfig(
@@ -25,9 +20,5 @@ class ArticleListRepository(private val FEED_TAG: Feed) {
                 ArticlePagingSource(query, sortBy, fromDate, FEED_TAG)
             }
         ).flowable
-    }
-
-    fun getArticleListLiveData(): MutableLiveData<Results> {
-        return articleListLiveData
     }
 }
